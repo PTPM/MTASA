582dacd89e194fb318981785e144dd1050d6eb25
Martin Loeffen <martin.loeffen@callisto.cc>
2016-10-07 15:25:53 +0200

    altered function to use PTPM colors in kill list

--- [util]/headshot/server.lua
+++ [util]/headshot/server.lua
@@ -31,8 +31,8 @@ addEventHandler("onPlayerDamage", getRootElement(), makeHeadshot)
 function outputHeadshot(killer, weapon, bodypart)
 	if bodypart == 9 and findNumber(headshot.allowedWeapons, weapon) then
 		cancelEvent()
-		local r2, g2, b2 = getTeamColor(getPlayerTeam(killer))
-		local r1, g1, b1 = getTeamColor(getPlayerTeam(source))
+		local r2, g2, b2, a2 = exports.ptpm:getPlayerColour(killer)
+		local r1, g1, b1, a1 = exports.ptpm:getPlayerColour(source)
 		exports.killmessages:outputMessage({getPlayerName(killer), {"padding", width=3}, {"icon", id=weapon}, {"padding", width=3}, {"icon", id=256}, {"padding", width=3}, {"color", r=r1, g=g1, b=b1}, getPlayerName(source)}, getRootElement(), r2, g2, b2)
 	end
 end
