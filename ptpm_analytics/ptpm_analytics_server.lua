5b0d0adb5f31e0ae092fef9a5c893673dee3ec19
Martin Loeffen <martin.loeffen@callisto.cc>
2016-10-29 13:32:12 +0200

    Placeholders

---
+++ ptpm_analytics/ptpm_analytics_server.lua
@@ -0,0 +1,28 @@
+-- PTPM NoSQL data mining
+
+-- File management
+
+
+-- Identification table (on join)
+-- datetime,<arbitrary rand id (aID; analytics ID)>,nick
+
+
+-- Geo logging (at 10 second interval)
+-- datetime,aID,class,x,y,z
+
+-- Log player deaths (on death)
+-- datetime,aID[vic],class[vic],x,y,z,aID[attacker],class[attacker]weapon,bodypart
+
+-- Log performance data (30 seconds after join)
+-- datetime,aID,ping,exports.admin.country,screenWidth,screenHeight,mtaVersion, dxGetStatus:VideoCardName,dxGetStatus:VideoCardRAM,dxGetStatus:VideoCardName,dxGetStatus:SettingWindowed,dxGetStatus:SettingDrawDistance,dxGetStatus:Setting32BitColor,dxGetStatus:SettingFOV
+
+
+
+
+
+
+
+
+
+
+
