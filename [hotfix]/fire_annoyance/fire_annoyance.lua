751a25eed27e2f5c1314ebf3a7c9fd2f3d581220
RempRemp <RempRemp@users.noreply.github.com>
2016-12-17 18:27:52 +0000

    Reworked fire_annoyance to disable firing with fire weapons around the spawn areas

--- [hotfix]/fire_annoyance/fire_annoyance.lua
+++ [hotfix]/fire_annoyance/fire_annoyance.lua
@@ -1,34 +1,33 @@
 local fireWeapons = {[18] = true, [37] = true} -- molotov, flame thrower
 local rd = 50 -- required distance from spawn to get the weapons
 local spawnX,spawnY = 0,0
-local lastRequest = 0
-
-local delayedWeapons = {}
 
 function registerSpawnArea()
 	spawnX, spawnY = getElementPosition(localPlayer)
+
+	toggleFireControl()
 end
 addEventHandler("onClientPlayerSpawn", localPlayer, registerSpawnArea)
 
-
-function disabledWeaponsInSpawn(weapon, ammo, ammoInClip, hitX, hitY, hitZ, hitElement)
-	if getPlayerTeam(localPlayer) == false then 
+function toggleFireControl()
+	if not getPlayerTeam(localPlayer) then 
 		return 
 	end
-	
-	if fireWeapons[weapon] then
-		local myPosX, myPosY = getElementPosition(localPlayer)
 
-		if getDistanceBetweenPoints2D(spawnX, spawnY, myPosX, myPosY) < rd and getTickCount() - lastRequest > 1000 then
-			triggerServerEvent("removeWeaponEvent", resourceRoot, weapon)
-			lastRequest = getTickCount()
+	local weapon = getPedWeapon(localPlayer)
+
+	if getPedTotalAmmo(localPlayer) == 0 then
+		return
+	end
+
+	local myPosX, myPosY = getElementPosition(localPlayer)
+
+	if fireWeapons[weapon] and getDistanceBetweenPoints2D(spawnX, spawnY, myPosX, myPosY) < rd then
+		toggleControl("fire", false)
+	else
+		if not isControlEnabled("fire") then
+			toggleControl("fire", true)
 		end
 	end
 end
-addEventHandler("onClientPlayerWeaponFire",  localPlayer, disabledWeaponsInSpawn)
-
--- addEventHandler("onClientResourceStart", resourceRoot,
--- 	function()
--- 		outputDebugString("fire_annoyance started")
--- 	end
--- )
+addEventHandler("onClientPlayerWeaponSwitch", localPlayer, toggleFireControl)
--- [hotfix]/fire_annoyance/fire_annoyance_server.lua
+++
@@ -1,7 +0,0 @@
-function removeWeaponReprimand(weaponId)
-	takeWeapon ( client, weaponId)
-	outputChatBox ( "ADMIN: Don't troll the spawn!", client, 255, 0, 0 )
-end
-
-addEvent( "removeWeaponEvent", true )
-addEventHandler( "removeWeaponEvent", resourceRoot, removeWeaponReprimand )
