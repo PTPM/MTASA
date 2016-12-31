a065c82b866096a82af927686d19dbdf63f0d65f
RempRemp <RempRemp@users.noreply.github.com>
2016-12-31 15:59:50 +0000

    Added experimental grouping to the joinquit messages. This groups together join/quit messages that happen very close to each other to minimise chat spam
    Example: "* Larry joined, Barry quit [Timed Out], Harry quit [Quit]"

--- [gameplay]/joinquit/joinquit.lua
+++ [gameplay]/joinquit/joinquit.lua
@@ -1,6 +1,26 @@
-﻿addEventHandler('onClientPlayerJoin', root,
+﻿local defaultColour = string.format("#%02x%02x%02x", 255, 100, 100)
+local joinColour = string.format("#%02x%02x%02x", 255, 150, 150)
+
+local cache = {
+	enabled = true,
+	messages = {}
+}
+
+addEventHandler("onClientResourceStart", resourceRoot,
 	function()
-		outputChatBox('* ' .. getPlayerName(source) .. ' has joined the game', 255, 100, 100)
+		if cache.enabled then
+			setTimer(outputMessageCache, 1000, 0)
+		end
+	end
+)
+
+addEventHandler('onClientPlayerJoin', root,
+	function()
+		if cache.enabled then
+			table.insert(cache.messages, joinColour .. getPlayerName(source) .. " joined")
+		else
+			outputChatBox('* ' .. getPlayerName(source) .. ' has joined the game', 255, 100, 100)
+		end
 	end
 )
 
@@ -12,6 +32,36 @@ addEventHandler('onClientPlayerChangeNick', root,
 
 addEventHandler('onClientPlayerQuit', root,
 	function(reason)
-		outputChatBox('* ' .. getPlayerName(source) .. ' has left the game [' .. reason .. ']', 255, 100, 100)
+		if cache.enabled then
+			-- kicks and bans bypass the cache
+			if reason == "Kicked" or reason == "Banned" then
+				outputChatBox('* ' .. getPlayerName(source) .. ' left [' .. reason .. ']', 255, 100, 100)
+			else
+				table.insert(cache.messages, defaultColour .. getPlayerName(source) .. " left [" .. reason .. "]")
+			end
+		else
+			outputChatBox('* ' .. getPlayerName(source) .. ' has left the game [' .. reason .. ']', 255, 100, 100)
+		end
 	end
 )
+
+function outputMessageCache()
+	if #cache.messages == 0 then
+		return
+	end
+
+	local output = "* "
+
+	for _, message in ipairs(cache.messages) do
+		if #output + #message > 256 then
+			outputChatBox(output, 255, 100, 100, true)
+			output = "* "
+		end
+
+		output = output .. (#output == 2 and "" or ", ") .. message
+	end
+
+	outputChatBox(output, 255, 100, 100, true)
+
+	cache.messages = {}
+end
