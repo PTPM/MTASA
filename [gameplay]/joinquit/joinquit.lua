46283e75122b0671c05e352cd816f4d877c3d6c1
RempRemp <RempRemp@users.noreply.github.com>
2016-12-31 15:55:52 +0000

    Unpacked joinquit so it can be edited

---
+++ [gameplay]/joinquit/joinquit.lua
@@ -0,0 +1,17 @@
+﻿addEventHandler('onClientPlayerJoin', root,
+	function()
+		outputChatBox('* ' .. getPlayerName(source) .. ' has joined the game', 255, 100, 100)
+	end
+)
+
+addEventHandler('onClientPlayerChangeNick', root,
+	function(oldNick, newNick)
+		outputChatBox('* ' .. oldNick .. ' is now known as ' .. newNick, 255, 100, 100)
+	end
+)
+
+addEventHandler('onClientPlayerQuit', root,
+	function(reason)
+		outputChatBox('* ' .. getPlayerName(source) .. ' has left the game [' .. reason .. ']', 255, 100, 100)
+	end
+)
