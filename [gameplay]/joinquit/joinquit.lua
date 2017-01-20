0083a48a1bf885d28012973bd597ee49ba5bddbf
Martin Loeffen <martin.loeffen@callisto.cc>
2017-01-20 12:40:55 +0100

    fix #24 (untested, no 2nd machine available)

--- [gameplay]/joinquit/joinquit.lua
+++ [gameplay]/joinquit/joinquit.lua
@@ -3,7 +3,8 @@ local joinColour = string.format("#%02x%02x%02x", 255, 150, 150)
 
 local cache = {
 	enabled = true,
-	messages = {}
+	messages = {},
+	stripColours = true
 }
 
 addEventHandler("onClientResourceStart", resourceRoot,
@@ -17,9 +18,9 @@ addEventHandler("onClientResourceStart", resourceRoot,
 addEventHandler('onClientPlayerJoin', root,
 	function()
 		if cache.enabled then
-			table.insert(cache.messages, joinColour .. getPlayerName(source) .. " joined")
+			table.insert(cache.messages, joinColour .. getColorStrippedPlayerName(source) .. " joined")
 		else
-			outputChatBox('* ' .. getPlayerName(source) .. ' has joined the game', 255, 100, 100)
+			outputChatBox('* ' .. getColorStrippedPlayerName(source) .. ' has joined the game', 255, 100, 100)
 		end
 	end
 )
@@ -35,12 +36,12 @@ addEventHandler('onClientPlayerQuit', root,
 		if cache.enabled then
 			-- kicks and bans bypass the cache
 			if reason == "Kicked" or reason == "Banned" then
-				outputChatBox('* ' .. getPlayerName(source) .. ' left [' .. reason .. ']', 255, 100, 100)
+				outputChatBox('* ' .. getColorStrippedPlayerName(source) .. ' left [' .. reason .. ']', 255, 100, 100)
 			else
-				table.insert(cache.messages, defaultColour .. getPlayerName(source) .. " left [" .. reason .. "]")
+				table.insert(cache.messages, defaultColour .. getColorStrippedPlayerName(source) .. " left [" .. reason .. "]")
 			end
 		else
-			outputChatBox('* ' .. getPlayerName(source) .. ' has left the game [' .. reason .. ']', 255, 100, 100)
+			outputChatBox('* ' .. getColorStrippedPlayerName(source) .. ' has left the game [' .. reason .. ']', 255, 100, 100)
 		end
 	end
 )
@@ -64,4 +65,27 @@ function outputMessageCache()
 	outputChatBox(output, 255, 100, 100, true)
 
 	cache.messages = {}
+end
+
+function getColorStrippedPlayerName(element)
+	if cache.stripColours then
+		return stripColourCodes(getPlayerName(element))
+	else
+		return getPlayerName(element)
+	end
+end
+
+function stripColourCodes(s)
+	local strippedString = s
+	local substitutions = 0
+
+	while true do
+		strippedString, substitutions = string.gsub(strippedString, "#%x%x%x%x%x%x", "")
+
+		if substitutions == 0 then
+			break
+		end
+	end
+
+	return strippedString
 end
