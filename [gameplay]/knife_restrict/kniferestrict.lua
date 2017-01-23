e5ef9da7de89ef56f74cad4fda3a9625d5de019f
Martin Loeffen <martin.loeffen@callisto.cc>
2017-01-23 17:44:51 +0100

    Tested and works, especially distance limit helps. Velocity was already part of GTASA it seems. Angle could be added later. I'm not good enough at math to implement that.

--- [gameplay]/knife_restrict/kniferestrict.lua
+++ [gameplay]/knife_restrict/kniferestrict.lua
@@ -1,4 +1,22 @@
-﻿function knifeRestrict(targetPlayer)
-    cancelEvent()
+﻿local killer = getLocalPlayer()
+
+function knifeRestrict(victim)
+	
+	-- A knife instakill is only allowed in some cases...
+	
+	-- 1) Distance between players < 1 meter
+	vicX,vicY,vicZ = getElementPosition(victim)
+	killerX,killerY,killerZ = getElementPosition(killer)
+	if getDistanceBetweenPoints3D(vicX,vicY,vicZ, killerX,killerY,killerZ) > 1 then cancelEvent() end
+	
+	-- 2) Victim and killer must be facing same direction (killer is facing back of victim, with a 45° leniency)
+	-- Not implemented
+	
+	-- 3) Victim must be standing still
+	vicSpeedX, vicSpeedY, vicSpeedZ = getElementVelocity(victim)
+	if math.sqrt(vicSpeedX^2 + vicSpeedY^2 + vicSpeedZ^2) > 1 then cancelEvent() end
+	
+	
+	
 end
 addEventHandler("onClientPlayerStealthKill", getLocalPlayer(), knifeRestrict)
