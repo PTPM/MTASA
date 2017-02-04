0d3fc2f1ec7c6593c31d18e19103c7ef1f8d6bc0
Martin Loeffen <martin.loeffen@callisto.cc>
2017-02-04 15:48:02 +0100

    Knife_restrict made server side + added angle check. UNTESTED AND INCLUDES DEBUG OUT!

--- [gameplay]/knife_restrict/kniferestrict.lua
+++ [gameplay]/knife_restrict/kniferestrict.lua
@@ -1,22 +1,46 @@
-﻿local killer = getLocalPlayer()
+﻿function normalizeAngles(degrees)
+	degrees = degrees % 360
+	if degrees < 360 then
+		degrees = degrees + 360
+	end
+	return degrees
+end
 
 function knifeRestrict(victim)
+	-- for readability:
+	local killer = source
 	
 	-- A knife instakill is only allowed in some cases...
 	
 	-- 1) Distance between players < 1 meter
 	vicX,vicY,vicZ = getElementPosition(victim)
 	killerX,killerY,killerZ = getElementPosition(killer)
-	if getDistanceBetweenPoints3D(vicX,vicY,vicZ, killerX,killerY,killerZ) > 1 then cancelEvent() end
+	if getDistanceBetweenPoints3D(vicX,vicY,vicZ, killerX,killerY,killerZ) > 1 then 
+		outputDebugString(getPlayerName(killer) .. " Knife kill prevented: distance too big", 1)
+		cancelEvent() 
+	end
 	
 	-- 2) Victim and killer must be facing same direction (killer is facing back of victim, with a 45° leniency)
 	-- Not implemented
+	_,_,vicRot = getElementRotation(victim)
+	_,_,killerRot = getElementRotation(killer)
+	
+	local minKillerAngle = normalizeAngles(vicRot - 90)
+	local maxKillerAngle = normalizeAngles(vicRot + 90)
+	
+	if killerRot < minKillerAngle or killerRot > maxKillerAngle then
+		outputDebugString(getPlayerName(killer) .. " Knife kill prevented: killerRot " .. killerRot .. " not in range of vicRot " .. vicRot , 1)
+		cancelEvent()
+	end
 	
 	-- 3) Victim must be standing still
 	vicSpeedX, vicSpeedY, vicSpeedZ = getElementVelocity(victim)
-	if math.sqrt(vicSpeedX^2 + vicSpeedY^2 + vicSpeedZ^2) > 1 then cancelEvent() end
+	if math.sqrt(vicSpeedX^2 + vicSpeedY^2 + vicSpeedZ^2) > 1 then 
+		outputDebugString(getPlayerName(killer) .. " Knife kill prevented: vic not standing still", 1)
+		cancelEvent() 
+	end
 	
 	
 	
 end
-addEventHandler("onClientPlayerStealthKill", getLocalPlayer(), knifeRestrict)
+addEventHandler("onPlayerStealthKill", getRootElement(), knifeRestrict)
