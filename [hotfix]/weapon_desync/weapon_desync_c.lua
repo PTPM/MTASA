70c5b9b8a85cb6242a4be53b7319550f44fcd88c
Martin Loeffen <martin.loeffen@callisto.cc>
2017-03-23 16:41:47 +0100

    weapon_desync #19 added onClientVehicleExit even though not really sure if needed

--- [hotfix]/weapon_desync/weapon_desync_c.lua
+++ [hotfix]/weapon_desync/weapon_desync_c.lua
@@ -8,3 +8,15 @@ addEventHandler("onClientPlayerWeaponFire", root, function(weapon, ammo, ammoInC
 		end
 	end
 end)
+
+addEventHandler("onClientVehicleExit", getRootElement(),
+    function(thePlayer, seat)
+        if thePlayer==localPlayer then
+			local weapon = 0 --getPedWeapon(localPlayer) (not actually needed since it turns out that you always leave a vehicle disarmed)
+			if weapon~=myLastWeapon then
+				triggerServerEvent ( "resyncMyWeapon", resourceRoot , weapon )
+				myLastWeapon = weapon
+			end
+		end
+    end
+)
