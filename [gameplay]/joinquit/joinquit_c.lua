40729d3c6a6d260aba6ec888c6771fd8ce6cd8d2
Martin Loeffen <martin.loeffen@callisto.cc>
2017-03-26 14:22:03 +0200

    unpack `admin` for change

--- [gameplay]/joinquit/joinquit_c.lua
+++ [gameplay]/joinquit/joinquit_c.lua
@@ -35,16 +35,17 @@ addEventHandler('onClientPlayerQuit', root,
 	function(reason)
 		if cache.enabled then
 			-- kicks and bans bypass the cache
-			if reason == "Kicked" or reason == "Banned" then
-				outputChatBox('* ' .. getColorStrippedPlayerName(source) .. ' left [' .. reason .. ']', 255, 100, 100)
+			if reason ~= "Kicked" and reason ~= "Banned" then
+				table.insert(cache.messages, defaultColour .. getColorStrippedPlayerName(source) .. " left (" .. reason .. ")")
 			else
-				table.insert(cache.messages, defaultColour .. getColorStrippedPlayerName(source) .. " left [" .. reason .. "]")
+				-- Kick and Ban messages are handled server side, because onClientPlayerQuit does not pass the actual reason, it passes the quitType and calls that "reason"
 			end
 		else
-			outputChatBox('* ' .. getColorStrippedPlayerName(source) .. ' has left the game [' .. reason .. ']', 255, 100, 100)
+			outputChatBox('* ' .. getColorStrippedPlayerName(source) .. ' has left the game (' .. reason .. ')', 255, 100, 100)
 		end
 	end
 )
+
 
 function outputMessageCache()
 	if #cache.messages == 0 then
--- [gameplay]/joinquit/joinquit_s.lua
+++ [gameplay]/joinquit/joinquit_s.lua
@@ -1,1 +1,22 @@
-﻿
+addEventHandler ( "onPlayerQuit", getRootElement(), function(quitType, reason, responsibleElement)
+	-- outputChatBox("JQ qt:" .. quitType .. " r:".. reason)
+
+	if quitType=="Banned" or quitType=="Kicked" then
+		
+		responsibleName = getPlayerName(responsibleElement) or "Console"
+		reason = reason or ""
+		responsibilitySuffix = ""
+		
+		if responsibleName~="Console" then
+			responsibilitySuffix = " by " .. responsibleName
+		end
+		
+		for _, v in ipairs( getElementsByType( "player" ) ) do
+			if v and isElement( v ) then
+				outputChatBox('* ' .. getPlayerName(source) .. ' was ' .. quitType:lower() .. responsibilitySuffix .. '. (' .. reason .. ')', v, 255, 100, 100)
+			end
+		end
+	
+	end
+end )
+
